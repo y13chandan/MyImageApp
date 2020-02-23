42c343b58adb865a315f2daf6b87d3abfab6a395
Chandan Kumar Singh <chandan.singh@industrybuying.com>
2020-02-23 21:03:39 +0530

    first commit

---
+++ app/src/main/java/com/example/myimageapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.myimageapp
+
+import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+
+        var width = displayMetrics.widthPixels
+        var height = displayMetrics.heightPixels
+
+        rl1.layoutParams.height = height - 60
+
+    }
+}
